6e865932e2bcd7a304b773e52769e9fc2a6ba3a9
RyougiShiki <805482013@qq.com>
2022-10-20 12:03:26 +1100

    added nodemon

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
