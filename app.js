5405f5a0bed975c8398f40e93023e58a61d18a4d
RyougiShiki <805482013@qq.com>
2022-10-21 14:43:53 +1100

    complete registration clicked register then stuck on the page, then web crashed

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mysql      = require('mysql');
+var bodyParser = require('body-parser');
 
-var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'ani',
+  password: 'password',
+  database: 'Ani'
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-app.post('/', (req, res) => {
-  res.send('Got a POST request')
+var userID = 1;
+app.post('/register', function(req, res){
+  var username = req.body.reg_username
+  var pwd = req.body.reg_password
+  userID = userID+1
+  var query = "INSERT INTO user (userName, pwd) VALUES ('" + 
+  username.toString() + "', '" + pwd.toString() + "'" + ");";
+  conn.connect(function(err){
+    if(err) throw err
+    console.log("Connected to database Ani")
+    conn.query(query, function(err, result){
+      if(err) throw err;
+      console.log(path.join('username: ', username.toString(), '  pwd: ', pwd.toString()))
+    }) 
+  })
+  console.log("registration complete")
 })
 
+const homeUrl = path.join(__dirname, '/public/home.html')
+app.get('/', (req, res) => {
+  res.sendFile(homeUrl)
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
