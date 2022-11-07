79839358a1007a41703b28dd03f6048acd767586
RyougiShiki <805482013@qq.com>
2022-11-07 20:55:07 +1100

    use createPool instead of createConnection

--- app.js
+++ app.js
@@ -5,36 +5,62 @@ const app = express()
 
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+const { stringify } = require('querystring');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
-var conn = mysql.createConnection({
+const conn = mysql.createPool({
   host: 'us-cdbr-east-06.cleardb.net',
   user: 'b8ea8ff3653c7b',
   password: '6c6b9c93',
   database: 'heroku_b623dcfae74a5b4'
 });
 
-conn.connect(function(err){
-  if(err) throw err
-  console.log("Connected to database Ani")
+conn.getConnection(function(err){
+    if(err) throw err;
+    console.log("Connected to database Ani");
+    var checkTable = "show tables like '%user%';";
+    var createTable = 
+        "CREATE TABLE user(userID int NOT NULL AUTO_INCREMENT, " + 
+        "userName VARCHAR(20) NOT NULL, " + 
+        "pwd VARCHAR(20) NOT NULL, " + 
+        "PRIMARY KEY(userID));";
+
+    conn.query(checkTable, function(err, result){
+        if(err){
+            throw err;
+            conn.query(createTable, function(err, result){
+                if(err) throw err;
+                console.log("table user created");
+            })
+        } 
+        console.log("table user exists");
+    })
 })
 
+const homeUrl = path.join(__dirname, '/public/home.html')
+app.get('/', function(req, res){
+  res.sendFile(homeUrl)
+})
+
+const registerUrl = path.join(__dirname, '/public/register.html')
+const loginUrl = path.join(__dirname, '/public/login.html')
+
 app.post('/register', function(req, res){
-  var username = req.body.reg_username
-  var pwd = req.body.reg_password
-  var query = "INSERT INTO user (userName, pwd) VALUES ('" + 
-  username.toString() + "', '" + pwd.toString() + "'" + ");";
+    var username = req.body.reg_username
+    var pwd = req.body.reg_password
+    var query = "INSERT INTO user (userName, pwd) VALUES ('" + 
+    username.toString() + "', '" + pwd.toString() + "'" + ");";
   
-  conn.query(query, function(err, result){
-    if(err) throw err;
-    console.log(path.join('username: ', username.toString(), '  pwd: ', pwd.toString()))
-    console.log("result: " + JSON.stringify(result))
-  }) 
-  console.log("registration complete")
-  res.sendFile(registerUrl)
+    conn.query(query, function(err, result){
+        if(err) throw err;
+        console.log(path.join('username: ', username.toString(), '  pwd: ', pwd.toString()))
+        console.log("result: " + JSON.stringify(result))
+    }) 
+    console.log("registration complete")
+    res.sendFile(registerUrl)
 })
 
 app.post('/login', function(req, res){
@@ -55,14 +81,6 @@ app.post('/login', function(req, res){
     }
   })
 })
-
-const homeUrl = path.join(__dirname, '/public/home.html')
-app.get('/', function(req, res){
-  res.sendFile(homeUrl)
-})
-
-const registerUrl = path.join(__dirname, '/public/register.html')
-const loginUrl = path.join(__dirname, '/public/login.html')
 
 const port = process.env.PORT || 3000;
 
