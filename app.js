5c75d6233f8887483ff70f24f0a2db8720fb9b88
RyougiShiki <805482013@qq.com>
2022-11-10 17:52:00 +1100

    can register, can't login. read data promise pending, null

--- app.js
+++ app.js
@@ -3,42 +3,16 @@ const http = require('http')
 const path = require('path')
 const app = express()
 
-var mysql = require('mysql');
+const MongoClient = require("mongodb").MongoClient;
 var bodyParser = require('body-parser');
-const { stringify } = require('querystring');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
-const conn = mysql.createPool({
-  host: 'us-cdbr-east-06.cleardb.net',
-  user: 'b8ea8ff3653c7b',
-  password: '6c6b9c93',
-  database: 'heroku_b623dcfae74a5b4'
-});
-
-conn.getConnection(function(err){
-    if(err) throw err;
-    console.log("Connected to database Ani");
-    var checkTable = "show tables like '%user%';";
-    var createTable = 
-        "CREATE TABLE user(userID int NOT NULL AUTO_INCREMENT, " + 
-        "userName VARCHAR(20) NOT NULL, " + 
-        "pwd VARCHAR(20) NOT NULL, " + 
-        "PRIMARY KEY(userID));";
-
-    conn.query(checkTable, function(err, result){
-        if(err){
-            throw err;
-            conn.query(createTable, function(err, result){
-                if(err) throw err;
-                console.log("table user created");
-            })
-        } 
-        console.log("table user exists");
-    })
-})
+const mongo_db_url = "mongodb+srv://root:7046406@cluster0.daoqxda.mongodb.net/test";
+const client = new MongoClient(mongo_db_url);
+client.connect();
 
 const homeUrl = path.join(__dirname, '/public/home.html')
 app.get('/', function(req, res){
@@ -51,21 +25,55 @@ const loginUrl = path.join(__dirname, '/public/login.html')
 app.post('/register', function(req, res){
     var username = req.body.reg_username
     var pwd = req.body.reg_password
-    var query = "INSERT INTO user (userName, pwd) VALUES ('" + 
-    username.toString() + "', '" + pwd.toString() + "'" + ");";
-  
-    conn.query(query, function(err, result){
-        if(err) throw err;
-        console.log(path.join('username: ', username.toString(), '  pwd: ', pwd.toString()))
-        console.log("result: " + JSON.stringify(result))
-    }) 
+    var register_obj = [
+      {
+        username: username,
+        password: pwd
+      }
+    ];
+    const db = client.db("Ani");
+    db.collection("user").insertMany(register_obj, function(err, res){
+    	if (err) throw err;
+    	console.log(res);
+    });
     console.log("registration complete")
     res.sendFile(registerUrl)
 })
 
+async function getUserInfo(login_obj){
+	try{
+		const db = client.db("Ani");
+		const coll = db.collection("user");
+		const login_info = await coll.find({}).toArray(function(err, result){
+			if (err) throw err;
+			console.log("login_info: "+ JSON.stringfy(result));
+		});
+		return login_info;
+	}	catch(error){
+		console.log(error);
+	}
+}
+
 app.post('/login', function(req, res){
   var username = req.body.login_username
   var pwd = req.body.login_password
+  var login_obj = 
+	{
+	  username: username,
+	  password: pwd
+	};
+  
+	const login_info = getUserInfo(login_obj);
+	console.log(login_info);
+	if (login_info.password = login_obj.password){
+		console.log("password equal");
+		res.sendFile(loginUrl);
+	} else {
+		console.log("password not equal");
+		res.end();
+	}
+	
+	/*
   var query = "SELECT pwd FROM user WHERE userName = '" + username.toString() + "';"
   console.log("start login")
   conn.query(query, function(err, result){
@@ -74,12 +82,13 @@ app.post('/login', function(req, res){
     var retPwd = JSON.parse(JSON.stringify(result))
     if (retPwd[0].pwd == pwd){
       console.log("user " + username + " login successfully")
-      res.sendFile(loginUrl)
+      
     } else {
       console.log("wrong password: " + retPwd[0].pwd)
-      res.end()
+      
     }
   })
+	*/
 })
 
 const port = process.env.PORT || 3000;
