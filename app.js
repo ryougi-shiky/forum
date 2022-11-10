5f20cbc9040814ef57717ee239f26a6c14c1d20b
RyougiShiki <805482013@qq.com>
2022-11-11 08:48:14 +1100

    login successful

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const http = require('http')
+const fs = require('fs')
 const path = require('path')
 const app = express()
+
 
 const MongoClient = require("mongodb").MongoClient;
 var bodyParser = require('body-parser');
@@ -40,38 +42,31 @@ app.post('/register', function(req, res){
     res.sendFile(registerUrl)
 })
 
-async function getUserInfo(login_obj){
-	try{
-		const db = client.db("Ani");
-		const coll = db.collection("user");
-		const login_info = await coll.find({}).toArray(function(err, result){
-			if (err) throw err;
-			console.log("login_info: "+ JSON.stringfy(result));
-		});
-		return login_info;
-	}	catch(error){
-		console.log(error);
-	}
-}
-
 app.post('/login', function(req, res){
   var username = req.body.login_username
   var pwd = req.body.login_password
-  var login_obj = 
-	{
-	  username: username,
-	  password: pwd
-	};
-  
-	const login_info = getUserInfo(login_obj);
-	console.log(login_info);
-	if (login_info.password = login_obj.password){
-		console.log("password equal");
-		res.sendFile(loginUrl);
-	} else {
-		console.log("password not equal");
-		res.end();
-	}
+
+	const db = client.db("Ani");
+	const coll = db.collection("user");
+	
+	coll.find({username: username}).toArray(function(err, result){
+		if (err){
+			console.log(err);
+			return;
+		}
+		console.log("result: ", result);
+		console.log("account password: ", result[0]["password"]);
+		console.log("entered password: ", pwd);
+		if (pwd === result[0]["password"]){
+			console.log("password equal");
+			res.sendFile(loginUrl);
+		} else {
+			console.log("password not equal");
+			res.redirect(req.get('host'));
+		}
+	});
+
+	
 	
 	/*
   var query = "SELECT pwd FROM user WHERE userName = '" + username.toString() + "';"
@@ -82,10 +77,8 @@ app.post('/login', function(req, res){
     var retPwd = JSON.parse(JSON.stringify(result))
     if (retPwd[0].pwd == pwd){
       console.log("user " + username + " login successfully")
-      
     } else {
       console.log("wrong password: " + retPwd[0].pwd)
-      
     }
   })
 	*/
