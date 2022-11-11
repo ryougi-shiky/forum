103c8d05634288c575a1afc0b0e7d9c4fbba06ef
RyougiShiki <805482013@qq.com>
2022-11-11 20:27:24 +1100

    split to route/home.js, register check redundant account

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
-
 const MongoClient = require("mongodb").MongoClient;
 var bodyParser = require('body-parser');
 
@@ -12,77 +11,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 
-const mongo_db_url = "mongodb+srv://root:7046406@cluster0.daoqxda.mongodb.net/test";
-const client = new MongoClient(mongo_db_url);
-client.connect();
-
-const homeUrl = path.join(__dirname, '/public/home.html')
+const homeUrl = path.join(__dirname, 'public/home.html');
 app.get('/', function(req, res){
   res.sendFile(homeUrl)
 })
 
-const registerUrl = path.join(__dirname, '/public/register.html')
-const loginUrl = path.join(__dirname, '/public/login.html')
+const login_register = require('./route/home.js')
+app.use('/', login_register);
 
-app.post('/register', function(req, res){
-    var username = req.body.reg_username
-    var pwd = req.body.reg_password
-    var register_obj = [
-      {
-        username: username,
-        password: pwd
-      }
-    ];
-    const db = client.db("Ani");
-    db.collection("user").insertMany(register_obj, function(err, res){
-    	if (err) throw err;
-    	console.log(res);
-    });
-    console.log("registration complete")
-    res.sendFile(registerUrl)
-})
-
-app.post('/login', function(req, res){
-  var username = req.body.login_username
-  var pwd = req.body.login_password
-
-	const db = client.db("Ani");
-	const coll = db.collection("user");
-	
-	coll.find({username: username}).toArray(function(err, result){
-		if (err){
-			console.log(err);
-			return;
-		}
-		console.log("result: ", result);
-		console.log("account password: ", result[0]["password"]);
-		console.log("entered password: ", pwd);
-		if (pwd === result[0]["password"]){
-			console.log("password equal");
-			res.sendFile(loginUrl);
-		} else {
-			console.log("password not equal");
-			res.redirect(req.get('host'));
-		}
-	});
-
-	
-	
-	/*
-  var query = "SELECT pwd FROM user WHERE userName = '" + username.toString() + "';"
-  console.log("start login")
-  conn.query(query, function(err, result){
-    if(err) throw err
-    console.log("result: " + JSON.stringify(result))
-    var retPwd = JSON.parse(JSON.stringify(result))
-    if (retPwd[0].pwd == pwd){
-      console.log("user " + username + " login successfully")
-    } else {
-      console.log("wrong password: " + retPwd[0].pwd)
-    }
-  })
-	*/
-})
 
 const port = process.env.PORT || 3000;
 
---
+++ route/home.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+
+const path = require('path')
+
+const MongoClient = require("mongodb").MongoClient;
+
+const mongo_db_url = "mongodb+srv://root:7046406@cluster0.daoqxda.mongodb.net/test";
+const client = new MongoClient(mongo_db_url);
+client.connect();
+
+const registerUrl = path.join(path.resolve(__dirname, '..'), '/public/register.html')
+const loginUrl = path.join(path.resolve(__dirname, '..'), '/public/login.html')
+
+router.post('/register', function(req, res){
+    var username = req.body.reg_username
+    var pwd = req.body.reg_password
+    var register_obj = [
+      {
+        username: username,
+        password: pwd
+      }
+    ];
+    const db = client.db("Ani");
+    db.collection("user").find({username: username}).toArray(function(err, result){
+		if (err) throw err;
+		console.log("result: ", result);
+        console.log("registering username: ", username);
+        if (result.length > 0){
+            console.log("account username: ", result[0]["username"]);
+            console.log("account exist");
+			res.redirect(req.get('host'));
+        } else {
+            console.log("account creating");
+            db.collection("user").insertMany(register_obj, function(err, res){
+                if (err) throw err;
+                console.log(res);
+            });
+            console.log("registration complete")
+            res.sendFile(registerUrl)
+        }
+	});
+})
+
+router.post('/login', function(req, res){
+  var username = req.body.login_username
+  var pwd = req.body.login_password
+
+	const db = client.db("Ani");
+	const coll = db.collection("user");
+	
+	coll.find({username: username}).toArray(function(err, result){
+		if (err){
+			console.log(err);
+			return;
+		}
+		console.log("result: ", result);
+		console.log("account password: ", result[0]["password"]);
+		console.log("entered password: ", pwd);
+		if (pwd === result[0]["password"]){
+			console.log("password equal");
+			res.sendFile(loginUrl);
+		} else {
+			console.log("password not equal");
+			res.redirect(req.get('host'));
+		}
+	});
+})
+
+module.exports = router;
