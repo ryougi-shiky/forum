67f18b5531f69365e1266e6e927220dc11f8c971
RyougiShiki <805482013@qq.com>
2023-02-03 17:47:41 +1100

    atlas is down

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,14 @@ import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <CookiesProvider>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
     </CookiesProvider>
     
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 
--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ const port = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}, () => {
+}, (err) => {
+  if (err) return err;
   console.log("Backend Server Connected to MongoDB");
 });
 
