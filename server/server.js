8a77f2a035bf0bc60c5eaff356148839beab6ffa
RyougiShiki <805482013@qq.com>
2023-07-17 10:58:04 +1000

    connect to new atlas.

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 dotenv.config({path: "./config.env"});
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO, {
+mongoose.connect(process.env.MONGO_ATLAS, {
   dbName: `ani`,
   useNewUrlParser: true,
   useUnifiedTopology: true
