cda56428af7bd4a08a6fd9a7aeff16f77db64322
RyougiShiki <53469345+ryougi-shiky@users.noreply.github.com>
2023-12-12 14:58:30 +0800

    creating gin

---
+++ server/repository/conndb.go
@@ -0,0 +1,8 @@
+package repository
+
+var user = "doadmin"
+var password = "AVNS_zP2kyuFUoqgF3b4k0AT"
+var server = "db-mysql-syd1-21907-do-user-14450765-0.c.db.ondigitalocean.com"
+var port = 25060
+var database = "defaultdb"
+
