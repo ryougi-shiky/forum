f7ac2f95846ec8e94b8bfc5b93efe5496b70017f
RyougiShiki <53469345+ryougi-shiky@users.noreply.github.com>
2023-12-12 22:31:38 +0800

    gin connect to mysql pod

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+
+	// "net/http"
+
+	// "github.com/gin-gonic/gin"
+	"server/repository"
+)
+
+func main() {
+	fmt.Println("Starting server...")
+
+	db, err := repository.ConnectToMysql()
+	if err != nil {
+		log.Fatalf("Failed to connect to DB: %s", err.Error())
+	}
+	log.Fatalf("Connected to DB: %s", db)
+	// userRepository := repository.NewUserRepository(db)
+	// log.Fatal(http.ListenAndServe(":8080", router))
 }
