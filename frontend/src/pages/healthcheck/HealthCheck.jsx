0929deae915b02b814b192b64bf7d79ce6e3ad3e
ryougi-shiky <yzm805482013@gmail.com>
2025-06-07 10:07:34 +0800

    chore: update HealthCheck.jsx

--- frontend/src/pages/healthcheck/HealthCheck.jsx
+++ frontend/src/pages/healthcheck/HealthCheck.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import Topbar from "../../components/topbar/Topbar";
 
 const backend_url = process.env.REACT_APP_BACKEND_URL;
 
@@ -7,20 +8,34 @@ export default function HealthCheck() {
     // const { user:currentUser, isFetching, error, dispatch, feed_display_moments } = useContext(AuthContext);
     const [backendResponse, setBackendResponse] = useState("");
     useEffect(() => {
+        let mounted = true;
+
         const healthCheckBackend = async () => {
             try {
                 const res = await axios.get(`${backend_url}/healthcheck/backend`);
-                setBackendResponse(res.data);
+                if (mounted) {
+                    setBackendResponse(res.data);
+                }
             } catch (err) {
-                setBackendResponse("Error fetching backend health.");
+                if (mounted) {
+                    setBackendResponse("Error fetching backend health.");
+                }
             }
         };
+
         healthCheckBackend();
-    }, [])
+
+        return () => {
+            mounted = false;
+        };
+    }, []);
 
     return (
-        <div>
-            <p>{backendResponse.message}</p>
-        </div>
+        <React.Fragment>
+            <Topbar/>
+            <div>
+                <p>{backendResponse.message}</p>
+            </div>
+        </React.Fragment>
     );
 }
