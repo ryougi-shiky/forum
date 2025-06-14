a8b89b714a41d24d94eebb6c6e1158951c122864
ryougi-shiky <yzm805482013@gmail.com>
2025-06-14 19:17:15 +0800

    feat: complte login e2e tests

--- frontend/cypress/e2e/login.cy.js
+++ frontend/cypress/e2e/login.cy.js
@@ -6,27 +6,30 @@ describe('Login Flow', () => {
   it('should login with correct credentials', () => {
     cy.visit(FRONTEND_URL_LOGIN)
 
-    cy.get('input[placeholder="Email"]').should('be.visible')
-    cy.get('input[placeholder="Password"]').should('be.visible')
+    cy.get('input[placeholder="Email"]').should('be.visible');
+    cy.get('input[placeholder="Password"]').should('be.visible');
 
-    cy.get('input[placeholder="Email"]').type('didi@gmail.com')
-    cy.get('input[placeholder="Password"]').type('yzm7046406')
+    cy.get('input[placeholder="Email"]').type('didi@gmail.com');
+    cy.get('input[placeholder="Password"]').type('yzm7046406');
 
-    cy.get('[data-testid="login-button"]').click()
+    cy.get('[data-testid="login-button"]').click();
 
-    cy.url().should('include', '/')
-    cy.get('.topbarImg').should('be.visible')
+    cy.url().should('eq', 'http://localhost:3000/');
+    cy.get('.topbarContainer').should('be.visible');
+    cy.get('.sidebar').should('be.visible');
+    cy.get('.feed').should('be.visible');
+    cy.get('.rightbar').should('be.visible');
   })
 
   it('should return 404 error with incorrect credentials', () => {
-    cy.intercept('POST', '**/login').as('loginRequest')
+    cy.intercept('POST', '**/login').as('loginRequest');
 
-    cy.visit(FRONTEND_URL_LOGIN)
+    cy.visit(FRONTEND_URL_LOGIN);
 
-    cy.get('input[placeholder="Email"]').type('wrong@email.com')
-    cy.get('input[placeholder="Password"]').type('wrongpass')
-    cy.get('[data-testid="login-button"]').click()
+    cy.get('input[placeholder="Email"]').type('wrong@email.com');
+    cy.get('input[placeholder="Password"]').type('wrongpass');
+    cy.get('[data-testid="login-button"]').click();
 
-    cy.wait('@loginRequest').its('response.statusCode').should('eq', 404)
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 404);
   })
 })
